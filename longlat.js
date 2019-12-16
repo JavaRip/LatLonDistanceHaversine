f7f4f7a3bc5bd9c2b7c808cbb1327da9ee5653dd
JavaRip <UP941226@myport.ac.uk>
2019-12-16 19:26:59 +0000

    readability improvements

--- longlat.js
+++ longlat.js
@@ -1,3 +1,4 @@
+//demo 
 const lon1Deg = 1.6551; 
 const lon2Deg = 1.5757; 
 const lat1Deg = 50.7531;
@@ -8,10 +9,10 @@ function getDistance(lon1Deg, lat1Deg, lon2Deg, lat2Deg) {
     //define location in degrees
     const radius = 6371e3; // radius of earth in meters
     //define locations in radians
-    const lat1Rad = lat1Deg * Math.PI / 180;
-    const lat2Rad = lon1Deg * Math.PI / 180;
-    const latDiffRad = (lat1Deg - lat2Deg) * Math.PI / 180;
-    const lonDiffRad = (lon2Deg - lon1Deg) * Math.PI / 180;
+    const lat1Rad = degToRad(lat1Deg);
+    const lat2Rad = degToRad(lon1Deg);
+    const latDiffRad = degToRad(lat1Deg - lat2Deg);
+    const lonDiffRad = degToRad(lon2Deg - lon1Deg);
     //haversine formula
     const haversine = getHaversine(latDiffRad, lonDiffRad, lat1Rad, lat2Rad);
     //get angular distance in radians
@@ -19,6 +20,10 @@ function getDistance(lon1Deg, lat1Deg, lon2Deg, lat2Deg) {
     //multiply angular distance in radians by the radius of the sphere it's on (earth)
     const distanceInMeters = radius * angularDistRad;
     return distanceInMeters;
+}
+
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
 }
 
 function getHaversine(latDiffRad, lonDiffRad, lat1Rad, lat2Rad) {
